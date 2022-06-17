07d16530288576eae052fd756a6a018c84db31cb
Miguel Condori <m16u3l_777@hotmail.com>
2022-06-17 17:54:53 -0400

    add http to test

--- src/services/university.service.js
+++ src/services/university.service.js
@@ -1,11 +1,11 @@
 // import axios from 'axios';
 import { getUniversityListExternal } from './external'
 
-const university_url = '//universities.hipolabs.com/search?name='
+const university_url = 'http//universities.hipolabs.com/search?name='
 
 const getUniversityList = async (name = "") => {
-  // const response = await axios.get(university_url + name);
-  const response = await getUniversityListExternal(name);
+  const response = await axios.get(university_url + name);
+  // const response = await getUniversityListExternal(name);
   return response;
 }
 
